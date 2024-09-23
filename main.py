12b25f016a9909d6e1fef0e7eeeefc65f2adc4d8
Smugcurve13 <sambhavsoni14@gmail.com>
2024-09-23 18:16:43 +0530

    added spaces in code

--- main.py
+++ main.py
@@ -15,20 +15,25 @@ while True:
             file = open('todos.txt', 'w')
             file.writelines(todos)
             file.close()
+
         case "show" :   #use "|" to add multiple words for a single case
             for index , item in enumerate(todos):
                 row = f"{index + 1}-{item}"
                 print(row)
+
         case 'edit':
             number = int(input("Number of the todo to edit:"))
             number = number - 1
             new_todo = input("enter new todo:")
             todos[number] = new_todo #use [] for index of todo in list
+
         case 'complete':
             number = int(input("Number of the todo to complete: "))
             todos.pop(number-1)
+
         case "exit":
             break
+        
         case _: #use random variable or _ for giving a command to a text that isnt mentioned in case 
             print("hey , you have entered an unknown command")
 
