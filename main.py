8499d7bb186cdba9b8343814b00b0372e49317f3
Smugcurve13 <sambhavsoni14@gmail.com>
2024-09-23 14:34:56 +0530

    todo v7.0 , added complete by pop

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 todos = []
 
 while True:
-    user_action = input("Type add, show, edit or exit: ")
+    user_action = input("Type add, show, edit, complete or exit: ")
     user_action = user_action.strip()
 
     match user_action:
@@ -10,14 +10,16 @@ while True:
             todos.append(todo)
         case "show" :   #use "|" to add multiple words for a single case
             for index , item in enumerate(todos):
-                row = f"{index}-{item}"
+                row = f"{index + 1}-{item}"
                 print(row)
         case 'edit':
             number = int(input("Number of the todo to edit:"))
             number = number - 1
             new_todo = input("enter new todo:")
             todos[number] = new_todo #use [] for index of todo in list
-            
+        case 'complete':
+            number = int(input("Number of the todo to complete: "))
+            todos.pop(number-1)
         case "exit":
             break
         case _: #use random variable or _ for giving a command to a text that isnt mentioned in case 
