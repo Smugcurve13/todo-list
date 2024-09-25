a544b77cf71fd1495f97976f3476bc1e05906b33
Smugcurve13 <sambhavsoni14@gmail.com>
2024-09-25 16:03:47 +0530

    added functionality to edit and complete

--- main.py
+++ main.py
@@ -3,17 +3,18 @@ while True:
     user_action = user_action.strip()
 
     if "add" in user_action:
-        todo = user_action[4:]
+
+        todo = user_action[4:] + '\n'
 
         with open('todos.txt','r') as file:
             todos = file.readlines()
 
-        todos.append(todo)
+        todos.append(todo) 
 
         with open('todos.txt', 'w') as file:
             file.writelines(todos)
 
-    if "show" in user_action:                                               #use "|" to add multiple words for a single case
+    elif "show" in user_action:                                               #use "|" to add multiple words for a single case
 
         with open('todos.txt','r') as file:
             todos = file.readlines()
@@ -26,8 +27,9 @@ while True:
             row = f"{index + 1}-{item}"
             print(row)
 
-    if "edit" in user_action:
-        number = int(input("Number of the todo to edit:"))
+    elif "edit" in user_action:
+
+        number = int(user_action[5:])
         number = number - 1
 
         with open('todos.txt','r') as file:
@@ -39,11 +41,13 @@ while True:
         with open('todos.txt', 'w') as file:
             file.writelines(todos)
 
-    if "complete" in user_action:
-        number = int(input("Number of the todo to complete: "))
+    elif "complete" in user_action:
+
+        number = int(user_action[9:])
         
         with open('todos.txt','r') as file:
             todos = file.readlines()
+
         index = number -1
         todo_to_remove = todos[index].strip('\n')
         todos.pop(index)
@@ -53,8 +57,10 @@ while True:
 
         message = f"Todo {todo_to_remove} was removed from the list"
         print(message)
-        
-    if "exit" in user_action:
+
+    elif "exit" in user_action:
         break
-        
+    else:
+        print("Command is not valid.")
+
 print("bye")
