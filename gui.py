1070b4199db5beeba36bf8bfcb86182f89767880
Smugcurve13 <sambhavsoni14@gmail.com>
2024-09-30 13:56:07 +0530

    modified gui.py, implmented Add Button and gui close button

--- gui.py
+++ gui.py
@@ -2,9 +2,23 @@ import functions
 import FreeSimpleGUI as gui
 
 label = gui.Text("Type in a to-do")
-input_box = gui.InputText(tooltip = "Enter Todo")
+input_box = gui.InputText(tooltip = "Enter Todo", key='todo')
 add_button = gui.Button("Add")
 
-window = gui.Window('MY to-do APP' , layout = [[label], [input_box , add_button]])
-window.read()
+window = gui.Window('My to-do App' , 
+                    layout = [[label], [input_box , add_button]],
+                    font = ('Helvetica', 11))
+while True:
+    event,values = window.read()
+    print(event)
+    print(values)
+    match event:
+        case "Add":
+            todos = functions.get_todos()
+            new_todo = values['todo'] + "\n"
+            todos.append(new_todo)
+            functions.write_todos(todos)
+        case gui.WIN_CLOSED:
+            break
+
 window.close()
